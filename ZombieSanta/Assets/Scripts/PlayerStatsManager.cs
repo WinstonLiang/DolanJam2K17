4846715e920ec8de992d62ad47f62a7f9b6f1606
Winston Liang <alakamande@gmail.com>
2017-12-28 20:36:29 -0800

    changes to stuff

--- ZombieSanta/Assets/Scripts/PlayerStatsManager.cs
+++ ZombieSanta/Assets/Scripts/PlayerStatsManager.cs
@@ -24,6 +24,7 @@ public class PlayerStatsManager : MonoBehaviour {
 
 		//game over
 		if (health <= 0){
+            Destroy(gameObject);
 			gameManager.GetComponent<GameManager> ().SwitchScene (2);
 		}
 	}
