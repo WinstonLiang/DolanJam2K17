985859dd56eecd574aaa75e9deab3718fd705183
Winston Liang <alakamande@gmail.com>
2017-12-28 20:17:14 -0800

    various fixes -snowballs implemented -trees layer correctly

---
+++ ZombieSanta/Assets/SelectColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectColor : MonoBehaviour {
+    public GameObject[] colors;
+	// Use this for initialization
+	void Start () {
+        int color = (int)(Random.Range(0f, 3f)-0.01);
+        if (color == 0)
+            colors[0].SetActive(true);
+        if (color == 1)
+            colors[1].SetActive(true);
+        if (color == 2)
+            colors[2].SetActive(true);
+	}
+}
