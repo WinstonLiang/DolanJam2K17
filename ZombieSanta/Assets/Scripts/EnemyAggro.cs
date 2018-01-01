2d055b5044ea4fd05ca4c75ccb582d866938084e
Winston Liang <alakamande@gmail.com>
2017-12-31 19:16:12 -0800

    changing aggro and chain aggroing and only 100 enemies in lvl 1 now

--- ZombieSanta/Assets/Scripts/EnemyAggro.cs
+++ ZombieSanta/Assets/Scripts/EnemyAggro.cs
@@ -4,23 +4,39 @@ using UnityEngine;
 
 public class EnemyAggro : MonoBehaviour {
 
+    public int chain;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start() {
+        chain = 0;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+    
+    void chainAggro(Collider2D other)
+    {
+        if (!gameObject.GetComponentInParent<chasePlayer>().aggro)
+        {
+            if (other.gameObject.tag == "Enemy")
+            {
+                if (other.gameObject.GetComponent<chasePlayer>() != null && other.gameObject.GetComponent<chasePlayer>().aggro
+                    && other.gameObject.GetComponentInChildren<EnemyAggro>().chain < 1)
+                {
+                    transform.parent.gameObject.GetComponent<chasePlayer>().aggro = true;
+                    transform.parent.gameObject.GetComponent<Animator>().SetBool("moving", true);
+                    chain++;
+                    transform.parent.gameObject.GetComponentInChildren<EnemyAggro>().chain++;
+                }
+            }
+        }
+    }
 
 	void OnTriggerEnter2D(Collider2D other) {
-
-		if(other.gameObject.tag == "Enemy") {
-			if(other.gameObject.GetComponent<chasePlayer>() != null && other.gameObject.GetComponent<chasePlayer>().aggro) {
-				transform.parent.gameObject.GetComponent<chasePlayer>().aggro = true;
-				transform.parent.gameObject.GetComponent<Animator>().SetBool("moving", true);
-			}
-		}
+        chainAggro(other);
 	}
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        chainAggro(other);
+    }
 }
